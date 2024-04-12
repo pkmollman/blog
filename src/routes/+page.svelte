8951a2e3fd24b8db54f275c6daa3984241d0a8a9
Patrick Mollman <pkmollman@gmail.com>
2024-04-12 18:48:29 -0400

    test again

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 
 <div class="w-8/12 m-auto">
 	<h1 class="text-3xl text-center text-primary-400 mt-6 underline">HOME</h1>
+	<h1 class="text-3xl text-center text-primary-400 mt-6 underline">test</h1>
 	<h1 class="text-3xl text-center text-primary-400">Latest Post</h1>
 	<PostLink postentry={posts[0]} />
 </div>
